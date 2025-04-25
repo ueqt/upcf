95f9fdd262a2b7d85084a2f92faaa437fc42f35c
ueqt <ueqt@ueqts-Mac-mini.local>
2025-04-25 12:44:09 +0800

    fix join

--- src/cli/libs/push/deal.js
+++ src/cli/libs/push/deal.js
@@ -1,4 +1,4 @@
-import { readFileSync, writeFileSync } from 'fs';
+import { readFileSync, writeFileSync, readdirSync } from 'fs';
 import shelljs from 'shelljs';
 import * as fixxml from '../fixxml/deal';
 
