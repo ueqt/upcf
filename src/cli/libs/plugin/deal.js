d70c7b3a3737b58d7cd6d66bc813934835762803
ueqt <ueqt@ueqts-Mac-mini.local>
2025-06-20 11:49:29 +0800

    fix plugins

--- src/cli/libs/plugin/deal.js
+++ src/cli/libs/plugin/deal.js
@@ -4,11 +4,7 @@ import { writeFileSync } from 'fs';
 const deal = async (solutionid) => {
   const schemaRequest = new SchemaRequest();
 
-  let plugins = await schemaRequest.request(`msdyn_solutioncomponentsummaries?%24filter=(msdyn_solutionid%20eq%20${solutionid})%20and%20((msdyn_componenttype%20eq%2092))&%24orderby=msdyn_displayname%20asc&api-version=9.1`, {
-    otherHeaders: {
-      Prefer: 'odata.include-annotations="*"'
-    }
-  });
+  let plugins = await schemaRequest.request(`msdyn_solutioncomponentsummaries?%24filter=(msdyn_solutionid%20eq%20${solutionid})%20and%20((msdyn_componenttype%20eq%2092))&%24orderby=msdyn_displayname%20asc&api-version=9.1`);
 
   console.log(`solutionid: ${solutionid}, plugins: ${plugins.length}`);
 
@@ -24,7 +20,6 @@ const deal = async (solutionid) => {
       Mode: plugin.msdyn_isolationmode,
       Stage: plugin.msdyn_executionstage,
       Message: plugin.msdyn_sdkmessagename,
-      Entity: plugin['_msdyn_objectid_value@OData.Community.Display.V1.FormattedValue'],
     }
 
     results.push(result);
